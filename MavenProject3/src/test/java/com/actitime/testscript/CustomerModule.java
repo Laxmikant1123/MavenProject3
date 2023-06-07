446f9854e9ad7d1d31aee0eccf7af823bf6836df
laxmi <laxmi@HP>
2023-06-07 18:15:33 +0530

    comitt

---
+++ MavenProject3/src/test/java/com/actitime/testscript/CustomerModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.junit.Test;
+import org.testng.Reporter;
+
+public class CustomerModule {
+		@Test
+		public void createCustomer() {
+			Reporter.log("CreateCustomer",true);
+	}
+
+}
